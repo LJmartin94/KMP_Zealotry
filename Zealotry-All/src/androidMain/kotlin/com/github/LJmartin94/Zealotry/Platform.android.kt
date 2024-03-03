fc0eef1e0e13d1ba27695219d779df8d7941751e
limartin <lindsayjamesmartin94@gmail.com>
2024-03-03 22:37:44 +0100

    Create project with Jetbrain's Kotlin Multiplatform Wizard instead.

--- Zealotry-All/src/androidMain/kotlin/com/github/LJmartin94/Zealotry/Platform.android.kt
+++
@@ -1,7 +0,0 @@
-package com.github.LJmartin94.Zealotry
-
-class AndroidPlatform : Platform {
-    override val name: String = "Android ${android.os.Build.VERSION.SDK_INT}"
-}
-
-actual fun getPlatform(): Platform = AndroidPlatform()
--- Zealotry-All/src/iosMain/kotlin/com/github/LJmartin94/Zealotry/Platform.ios.kt
+++
@@ -1,9 +0,0 @@
-package com.github.LJmartin94.Zealotry
-
-import platform.UIKit.UIDevice
-
-class IOSPlatform: Platform {
-    override val name: String = UIDevice.currentDevice.systemName() + " " + UIDevice.currentDevice.systemVersion
-}
-
-actual fun getPlatform(): Platform = IOSPlatform()
