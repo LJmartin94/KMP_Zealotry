85e0e2116b31f136165b5d7e86730db58850526f
Lindsay Martin <lindsayjamesmartin94@gmail.com>
2024-03-31 21:58:19 +0200

    Just get rid of some hardcoded strings

--- composeApp/src/commonMain/kotlin/presentation/screens/mainMenu/MainMenuScreen.kt
+++ composeApp/src/commonMain/kotlin/presentation/screens/mainMenu/MainMenuScreen.kt
@@ -13,20 +13,27 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import org.jetbrains.compose.resources.ExperimentalResourceApi
+import org.jetbrains.compose.resources.stringResource
+import zealotry.composeapp.generated.resources.Res
+import zealotry.composeapp.generated.resources.day
+import zealotry.composeapp.generated.resources.evening
+import zealotry.composeapp.generated.resources.morning
 
+@OptIn(ExperimentalResourceApi::class)
 @Composable
 fun MainMenuScreen(viewModel: MainMenuViewModel, onNavigate: (String)->Unit) {
     val uiState by viewModel.uiState.collectAsState()
 
     Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally){
         Text(
-            text = "Placeholder MorningMenu",
+            text = "Placeholder MainMenu",
             modifier = Modifier.padding(20.dp),
             fontSize = 24.sp,
             textAlign = TextAlign.Center
         )
-        OutlinedButton(onClick = {onNavigate("Morning")}){ Text("Morning") }
-        OutlinedButton(onClick = {onNavigate("Day")}){ Text("Day") }
-        OutlinedButton(onClick = {onNavigate("Evening")}){ Text("Evening") }
+        OutlinedButton(onClick = {onNavigate("Morning")}){ Text(text=stringResource(Res.string.morning)) }
+        OutlinedButton(onClick = {onNavigate("Day")}){ Text(text=stringResource(Res.string.day)) }
+        OutlinedButton(onClick = {onNavigate("Evening")}){ Text(text=stringResource(Res.string.evening)) }
     }
 }
