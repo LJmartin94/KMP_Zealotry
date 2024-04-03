a96d772b6d4593071d36aaa4424981e3603c5732
Lindsay Martin <lindsayjamesmartin94@gmail.com>
2024-04-03 22:09:06 +0200

    Separate Main Menu Buttons into separate file, add background image to them

---
+++ composeApp/src/commonMain/kotlin/presentation/screens/mainMenu/MainMenuButton.kt
@@ -0,0 +1,37 @@
+package presentation.screens.mainMenu
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.paint
+import androidx.compose.ui.layout.ContentScale
+import org.jetbrains.compose.resources.DrawableResource
+import org.jetbrains.compose.resources.ExperimentalResourceApi
+import org.jetbrains.compose.resources.StringResource
+import org.jetbrains.compose.resources.painterResource
+import org.jetbrains.compose.resources.stringResource
+
+@OptIn(ExperimentalResourceApi::class)
+@Composable
+fun MainMenuButton(
+    modifier: Modifier = Modifier,
+    navOp: () -> Unit,
+    textRes: StringResource,
+    backgroundRes: DrawableResource
+) =
+    Box(
+        modifier = modifier.clickable { navOp() }
+            .paint(
+                painterResource(backgroundRes),
+                contentScale = ContentScale.Crop
+            )
+    ) {
+        Text(
+            text = stringResource(textRes),
+            modifier = Modifier.align(Alignment.BottomCenter)
+        )
+    }
