06285c62ae6609b189088253a761c8868a52fbbe
Lindsay Martin <lindsayjamesmartin94@gmail.com>
2024-04-03 22:10:29 +0200

    Try to make layout work and respect individual item weights, but it is very obviously broken in landscape with the current setup

--- composeApp/src/commonMain/kotlin/presentation/screens/mainMenu/MainMenuScreen.kt
+++ composeApp/src/commonMain/kotlin/presentation/screens/mainMenu/MainMenuScreen.kt
@@ -5,7 +5,6 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.OutlinedButton
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -19,12 +18,14 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import org.jetbrains.compose.resources.ExperimentalResourceApi
 import org.jetbrains.compose.resources.painterResource
-import org.jetbrains.compose.resources.stringResource
 import zealotry.composeapp.generated.resources.Res
 import zealotry.composeapp.generated.resources.winter
 import zealotry.composeapp.generated.resources.day
+import zealotry.composeapp.generated.resources.day_button
 import zealotry.composeapp.generated.resources.evening
+import zealotry.composeapp.generated.resources.evening_button
 import zealotry.composeapp.generated.resources.morning
+import zealotry.composeapp.generated.resources.morning_button
 
 @OptIn(ExperimentalResourceApi::class)
 @Composable
@@ -37,15 +38,45 @@ fun MainMenuScreen(viewModel: MainMenuViewModel, onNavigate: (String) -> Unit) {
             contentScale = ContentScale.Crop
         )
     })
-    Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
+
+    Column(
+        Modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
         Text(
             text = "Placeholder MainMenu",
-            modifier = Modifier.padding(20.dp),
+            modifier = Modifier.padding(20.dp).weight(1f),
             fontSize = 24.sp,
             textAlign = TextAlign.Center
         )
-        OutlinedButton(onClick = { onNavigate("Morning") }) { Text(text = stringResource(Res.string.morning)) }
-        OutlinedButton(onClick = { onNavigate("Day") }) { Text(text = stringResource(Res.string.day)) }
-        OutlinedButton(onClick = { onNavigate("Evening") }) { Text(text = stringResource(Res.string.evening)) }
+
+        Column(
+            Modifier.weight(3f).fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+
+            MainMenuButton(
+                modifier = Modifier.fillMaxWidth().weight(1f),
+                navOp = { onNavigate("Morning") },
+                textRes = Res.string.morning,
+                backgroundRes = Res.drawable.morning_button
+            )
+
+            MainMenuButton(
+                modifier = Modifier.fillMaxWidth().weight(1f),
+                navOp = { onNavigate("Day") },
+                textRes = Res.string.day,
+                backgroundRes = Res.drawable.day_button
+            )
+
+            MainMenuButton(
+                modifier = Modifier.fillMaxWidth().weight(1f),
+                navOp = { onNavigate("Evening") },
+                textRes = Res.string.evening,
+                backgroundRes = Res.drawable.evening_button
+            )
+
+        }
     }
 }
