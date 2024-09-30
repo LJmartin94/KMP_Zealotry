4584ec4871b4fee953df6074ab82ebd88488b1ef
Lindsay Martin <lindsayjamesmartin94@gmail.com>
2024-09-30 21:18:59 +0200

    Store colours of Light Theme as named constants in MainColours.kt for readability

--- composeApp/src/commonMain/kotlin/presentation/style/MainColours.kt
+++ composeApp/src/commonMain/kotlin/presentation/style/MainColours.kt
@@ -6,23 +6,50 @@ import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 
+//Light Theme (default):
+val CHARCOAL = Color(0xFF262626)
+val DARK_GREY = Color(0xFFAFADAA)
+val GREY = Color(0xFFCFCECC)
+val PALE_GREY = Color(0xFFECEBEA)
+val WHITE_ISH = Color(0xFFFAFAFA)
+val DARK_ORANGE = Color(0xFFFF9201)
+val ORANGE = Color(0xFFFF9D1A)
+val RED = Color(0xFFFF1201)
+
+
 @Composable
 fun MainColours() =
     if (isSystemInDarkTheme()) {
         darkColors()
     } else {
         lightColors(
-            primary = Color(0xFFAFADAA),
-            primaryVariant = Color(0xFFCFCECC),
-            secondary = Color(0xFFFF9201),
-            secondaryVariant = Color(0xFFFF9D1A),
-            background = Color(0xFFECEBEA),
-            surface = Color(0xFFECEBEA),
-            error = Color(0xFFFF1201),
-            onPrimary = Color(0xFF262626),
-            onSecondary = Color(0xFFFAFAFA),
-            onBackground = Color(0xFF262626),
-            onSurface = Color(0xFF262626),
-            onError = Color(0xFFFAFAFA),
+            primary = DARK_GREY,
+            primaryVariant = GREY,
+            secondary = DARK_ORANGE,
+            secondaryVariant = ORANGE,
+            background = PALE_GREY,
+            surface = PALE_GREY,
+            error = RED,
+            onPrimary = CHARCOAL,
+            onSecondary = WHITE_ISH,
+            onBackground = CHARCOAL,
+            onSurface = CHARCOAL,
+            onError = WHITE_ISH,
         )
     }
+
+/* Default light:
+*     primary: Color = Color(0xFF6200EE),
+    primaryVariant: Color = Color(0xFF3700B3),
+    secondary: Color = Color(0xFF03DAC6),
+    secondaryVariant: Color = Color(0xFF018786),
+    background: Color = Color.White,
+    surface: Color = Color.White,
+    error: Color = Color(0xFFB00020),
+    onPrimary: Color = Color.White,
+    onSecondary: Color = Color.Black,
+    onBackground: Color = Color.Black,
+    onSurface: Color = Color.Black,
+    onError: Color = Color.White
+*
+*/
