a42834fad41c0dd431881ad58cfdbaee0b20125d
Lindsay Martin <lindsayjamesmartin94@gmail.com>
2024-11-11 17:27:30 +0100

    Tweaked/Fixed some colours in theme, dark mode didn't look right

--- composeApp/src/commonMain/kotlin/presentation/style/MainColours.kt
+++ composeApp/src/commonMain/kotlin/presentation/style/MainColours.kt
@@ -24,32 +24,32 @@ const val RED = 0xFFFF1201
 fun MainColours() =
     if (isSystemInDarkTheme()) {
         darkColors(
-            primary = Color(BROWN_ORANGE),
-            primaryVariant = Color(DARKEST_CHARCOAL),
+            primary = Color(DARK_GREY),
+            primaryVariant = Color(CHARCOAL),
+            onPrimary = Color(WHITE_ISH),
             secondary = Color(BROWN_ORANGE),
             secondaryVariant = Color(DARK_SAND),
-            background = Color(DARK_GREY),
-            surface = Color(DARK_GREY),
-            error = Color(MUTED_RED),
-            onPrimary = Color.White,
             onSecondary = Color.White,
-            onBackground = Color(CHARCOAL),
-            onSurface = Color(CHARCOAL),
+            background = Color(DARKEST_CHARCOAL),
+            onBackground = Color(WHITE_ISH),
+            surface = Color(DARKEST_CHARCOAL),
+            onSurface = Color(WHITE_ISH),
+            error = Color(MUTED_RED),
             onError = Color.Black,
         )
     } else {
         lightColors(
             primary = Color(DARK_GREY),
             primaryVariant = Color(GREY),
+            onPrimary = Color(CHARCOAL),
             secondary = Color(DARK_ORANGE),
             secondaryVariant = Color(ORANGE),
-            background = Color(PALE_GREY),
-            surface = Color(PALE_GREY),
-            error = Color(RED),
-            onPrimary = Color(CHARCOAL),
             onSecondary = Color(WHITE_ISH),
+            background = Color(PALE_GREY),
             onBackground = Color(CHARCOAL),
+            surface = Color(PALE_GREY),
             onSurface = Color(CHARCOAL),
+            error = Color(RED),
             onError = Color(WHITE_ISH),
         )
     }
