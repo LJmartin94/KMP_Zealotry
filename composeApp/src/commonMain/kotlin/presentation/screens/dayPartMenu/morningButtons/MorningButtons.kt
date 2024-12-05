6f0b91107a66f35ae92c2fc356d485b5270919fe
Lindsay Martin <lindsayjamesmartin94@gmail.com>
2024-12-05 22:43:43 +0100

    Make morning menu buttons use ChiaroscuroDrawable

--- composeApp/src/commonMain/kotlin/presentation/screens/dayPartMenu/morningButtons/MorningButtons.kt
+++ composeApp/src/commonMain/kotlin/presentation/screens/dayPartMenu/morningButtons/MorningButtons.kt
@@ -1,44 +1,63 @@
 package presentation.screens.dayPartMenu.morningButtons
 
 import org.jetbrains.compose.resources.ExperimentalResourceApi
+import presentation.reusableUi.ChiaroscuroDrawable
 import presentation.reusableUi.IconTextTimeBundle
 import zealotry.composeapp.generated.resources.Res
 import zealotry.composeapp.generated.resources.asclepius_small
+import zealotry.composeapp.generated.resources.asclepius_small_inv
 import zealotry.composeapp.generated.resources.bins
 import zealotry.composeapp.generated.resources.bins_small
+import zealotry.composeapp.generated.resources.bins_small_inv
 import zealotry.composeapp.generated.resources.breakfast
 import zealotry.composeapp.generated.resources.brush_teeth
 import zealotry.composeapp.generated.resources.coffee
 import zealotry.composeapp.generated.resources.cosmeticism
 import zealotry.composeapp.generated.resources.curtains_small
+import zealotry.composeapp.generated.resources.curtains_small_inv
 import zealotry.composeapp.generated.resources.dishes
 import zealotry.composeapp.generated.resources.energy_rune_small
+import zealotry.composeapp.generated.resources.energy_rune_small_inv
 import zealotry.composeapp.generated.resources.get_up
 import zealotry.composeapp.generated.resources.grain_jar_small
+import zealotry.composeapp.generated.resources.grain_jar_small_inv
 import zealotry.composeapp.generated.resources.health_rune_small
+import zealotry.composeapp.generated.resources.health_rune_small_inv
 import zealotry.composeapp.generated.resources.imix_small
+import zealotry.composeapp.generated.resources.imix_small_inv
 import zealotry.composeapp.generated.resources.language
 import zealotry.composeapp.generated.resources.language_small
+import zealotry.composeapp.generated.resources.language_small_inv
 import zealotry.composeapp.generated.resources.lotus_watered_small
+import zealotry.composeapp.generated.resources.lotus_watered_small_inv
 import zealotry.composeapp.generated.resources.make_bed
 import zealotry.composeapp.generated.resources.meditation
 import zealotry.composeapp.generated.resources.meditation_small
+import zealotry.composeapp.generated.resources.meditation_small_inv
 import zealotry.composeapp.generated.resources.news
 import zealotry.composeapp.generated.resources.news_small
+import zealotry.composeapp.generated.resources.news_small_inv
 import zealotry.composeapp.generated.resources.open_curtains
 import zealotry.composeapp.generated.resources.pack_bag
 import zealotry.composeapp.generated.resources.pack_small
+import zealotry.composeapp.generated.resources.pack_small_inv
 import zealotry.composeapp.generated.resources.perfume_small
+import zealotry.composeapp.generated.resources.perfume_small_inv
 import zealotry.composeapp.generated.resources.posession_stone_small
+import zealotry.composeapp.generated.resources.posession_stone_small_inv
 import zealotry.composeapp.generated.resources.shower
 import zealotry.composeapp.generated.resources.smoothie
 import zealotry.composeapp.generated.resources.tooth_alt_small
+import zealotry.composeapp.generated.resources.tooth_alt_small_inv
 import zealotry.composeapp.generated.resources.vitamins
 import zealotry.composeapp.generated.resources.wake_up
 import zealotry.composeapp.generated.resources.wake_up_small
+import zealotry.composeapp.generated.resources.wake_up_small_inv
 import zealotry.composeapp.generated.resources.water
 import zealotry.composeapp.generated.resources.water_alt_small
+import zealotry.composeapp.generated.resources.water_alt_small_inv
 import zealotry.composeapp.generated.resources.water_bath_small
+import zealotry.composeapp.generated.resources.water_bath_small_inv
 import zealotry.composeapp.generated.resources.water_plants
 
 enum class MorningButtons {
@@ -69,97 +88,97 @@ fun MorningButtons.toBundle(): IconTextTimeBundle =
     when (this) {
         MorningButtons.WAKE_UP ->
             IconTextTimeBundle(
-                iconRes = Res.drawable.wake_up_small,
+                chiaro = ChiaroscuroDrawable(Res.drawable.wake_up_small, Res.drawable.wake_up_small_inv),
                 textRes = Res.string.wake_up,
             )
         MorningButtons.GET_UP ->
             IconTextTimeBundle(
-                iconRes = Res.drawable.wake_up_small,
+                chiaro = ChiaroscuroDrawable(Res.drawable.wake_up_small, Res.drawable.wake_up_small_inv),
                 textRes = Res.string.get_up,
             )
         MorningButtons.SHOWER ->
             IconTextTimeBundle(
-                iconRes = Res.drawable.water_bath_small,
+                chiaro = ChiaroscuroDrawable(Res.drawable.water_bath_small, Res.drawable.water_bath_small_inv),
                 textRes = Res.string.shower,
             )
         MorningButtons.NEWS ->
             IconTextTimeBundle(
-                iconRes = Res.drawable.news_small,
+                chiaro = ChiaroscuroDrawable(Res.drawable.news_small, Res.drawable.news_small_inv),
                 textRes = Res.string.news,
             )
         MorningButtons.OPEN_CURTAINS ->
             IconTextTimeBundle(
-                iconRes = Res.drawable.curtains_small,
+                chiaro = ChiaroscuroDrawable(Res.drawable.curtains_small, Res.drawable.curtains_small_inv),
                 textRes = Res.string.open_curtains,
             )
         MorningButtons.COFFEE ->
             IconTextTimeBundle(
-                iconRes = Res.drawable.energy_rune_small,
+                chiaro = ChiaroscuroDrawable(Res.drawable.energy_rune_small, Res.drawable.energy_rune_small_inv),
                 textRes = Res.string.coffee,
             )
         MorningButtons.SMOOTHIE ->
             IconTextTimeBundle(
-                iconRes = Res.drawable.health_rune_small,
+                chiaro = ChiaroscuroDrawable(Res.drawable.health_rune_small, Res.drawable.health_rune_small_inv),
                 textRes = Res.string.smoothie,
             )
         MorningButtons.WATER ->
             IconTextTimeBundle(
-                iconRes = Res.drawable.water_alt_small,
+                chiaro = ChiaroscuroDrawable(Res.drawable.water_alt_small, Res.drawable.water_alt_small_inv),
                 textRes = Res.string.water,
             )
         MorningButtons.VITAMINS ->
             IconTextTimeBundle(
-                iconRes = Res.drawable.asclepius_small,
+                chiaro = ChiaroscuroDrawable(Res.drawable.asclepius_small, Res.drawable.asclepius_small_inv),
                 textRes = Res.string.vitamins,
             )
         MorningButtons.DISHES ->
             IconTextTimeBundle(
-                iconRes = Res.drawable.posession_stone_small,
+                chiaro = ChiaroscuroDrawable(Res.drawable.posession_stone_small, Res.drawable.posession_stone_small_inv),
                 textRes = Res.string.dishes,
             )
         MorningButtons.BREAKFAST ->
             IconTextTimeBundle(
-                iconRes = Res.drawable.grain_jar_small,
+                chiaro = ChiaroscuroDrawable(Res.drawable.grain_jar_small, Res.drawable.grain_jar_small_inv),
                 textRes = Res.string.breakfast,
             )
         MorningButtons.WATER_PLANTS ->
             IconTextTimeBundle(
-                iconRes = Res.drawable.lotus_watered_small,
+                chiaro = ChiaroscuroDrawable(Res.drawable.lotus_watered_small, Res.drawable.lotus_watered_small_inv),
                 textRes = Res.string.water_plants,
             )
         MorningButtons.MEDITATION ->
             IconTextTimeBundle(
-                iconRes = Res.drawable.meditation_small,
+                chiaro = ChiaroscuroDrawable(Res.drawable.meditation_small, Res.drawable.meditation_small_inv),
                 textRes = Res.string.meditation,
             )
         MorningButtons.LANGUAGE ->
             IconTextTimeBundle(
-                iconRes = Res.drawable.language_small,
+                chiaro = ChiaroscuroDrawable(Res.drawable.language_small, Res.drawable.language_small_inv),
                 textRes = Res.string.language,
             )
         MorningButtons.BRUSH_TEETH ->
             IconTextTimeBundle(
-                iconRes = Res.drawable.tooth_alt_small,
+                chiaro = ChiaroscuroDrawable(Res.drawable.tooth_alt_small, Res.drawable.tooth_alt_small_inv),
                 textRes = Res.string.brush_teeth,
             )
         MorningButtons.COSMETICISM ->
             IconTextTimeBundle(
-                iconRes = Res.drawable.perfume_small, // TODO: change
+                chiaro = ChiaroscuroDrawable(Res.drawable.perfume_small, Res.drawable.perfume_small_inv),
                 textRes = Res.string.cosmeticism,
             )
         MorningButtons.PACK_BAG ->
             IconTextTimeBundle(
-                iconRes = Res.drawable.pack_small,
+                chiaro = ChiaroscuroDrawable(Res.drawable.pack_small, Res.drawable.pack_small_inv),
                 textRes = Res.string.pack_bag,
             )
         MorningButtons.MAKE_BED ->
             IconTextTimeBundle(
-                iconRes = Res.drawable.imix_small,
+                chiaro = ChiaroscuroDrawable(Res.drawable.imix_small, Res.drawable.imix_small_inv),
                 textRes = Res.string.make_bed,
             )
         MorningButtons.BINS ->
             IconTextTimeBundle(
-                iconRes = Res.drawable.bins_small,
+                chiaro = ChiaroscuroDrawable(Res.drawable.bins_small, Res.drawable.bins_small_inv),
                 textRes = Res.string.bins,
             )
     }
