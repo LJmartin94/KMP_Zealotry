489e7807390ae711f916cba39ec06e6f3d670357
Lindsay Martin <lindsayjamesmartin94@gmail.com>
2025-03-23 22:09:07 +0100

    Following Tutorial: Create MongoDB class in data layer

---
+++ composeApp/src/commonMain/kotlin/data/tutorial/MongoDB.kt
@@ -0,0 +1,48 @@
+package data.tutorial
+
+import domain.tutorial.RequestState
+import domain.tutorial.ToDoTask
+import io.realm.kotlin.Realm
+import io.realm.kotlin.RealmConfiguration
+import io.realm.kotlin.ext.query
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.map
+
+class MongoDB {
+    var realm: Realm? = null
+
+    private fun configureTheRealm(){
+        if (realm == null || realm!!.isClosed()){
+            // Pass all collection Model classes here.
+            val config = RealmConfiguration.Builder(
+                schema = setOf(ToDoTask::class)
+            )
+                .compactOnLaunch()
+                .build()
+            realm = Realm.open(config)
+        }
+    }
+
+    fun readAllTasks(): Flow<RequestState<List<ToDoTask>>> {
+        return realm?.query<ToDoTask>(query = "completed == $0", false)
+            ?.asFlow()
+            ?.map { result ->
+                RequestState.Success(
+                    data = result.list.sortedByDescending { task -> task.favourite }
+                )
+            } ?: flow { RequestState.Error(message = "Realm is not available.")}
+    }
+
+    fun readCompletedTasks(): Flow<RequestState<List<ToDoTask>>> {
+        return realm?.query<ToDoTask>(query = "completed == $0", true)
+            ?.asFlow()
+            ?.map { result ->
+                RequestState.Success(data = result.list)
+            } ?: flow { RequestState.Error(message = "Realm is not available.")}
+    }
+
+    suspend fun addTask(task: ToDoTask) {
+        realm?.write { copyToRealm(task) }
+    }
+}
