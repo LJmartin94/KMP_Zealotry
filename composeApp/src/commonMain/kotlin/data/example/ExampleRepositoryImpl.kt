e847be495f5c1dc5d0921c0ff6ea7d91fc6ef6dc
Lindsay Martin <lindsayjamesmartin94@gmail.com>
2026-01-20 17:47:45 +0100

    Add dummy implementation placeholder code for what ExampleRepositoryImpl might actually return

--- composeApp/src/commonMain/kotlin/data/example/ExampleRepositoryImpl.kt
+++ composeApp/src/commonMain/kotlin/data/example/ExampleRepositoryImpl.kt
@@ -3,7 +3,24 @@ package data.example
 class ExampleRepositoryImpl(
 //    private val networkDataSource: NetworkDataSource,
 //    private val localDataSource: ExampleDao,
-) {
+) : ExampleRepository {
+
+    //PLACEHOLDER CODE FOR WHAT A DB MIGHT ACTUALLY RETURN:
+    init {
+        println("Initialised Example Repository")
+    }
+
+    var id: String = ""
+    var toggleState: Boolean = false
+
+    override suspend fun getExample(): Result<Example> {
+        return Result.success(Example(id, toggleState))
+    }
+
+    override suspend fun updateToggle(toggle: Boolean): Result<Unit> {
+        toggleState = toggle
+        return Result.success(Unit)
+    }
 }
 
 //TODO: Complete the data layer implementation for example based on
