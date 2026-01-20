818a85ab68877fa286ebc73c8e5c3cdcdf1d184d
Lindsay Martin <lindsayjamesmartin94@gmail.com>
2026-01-20 12:33:09 +0100

    Add ExampleViewModel

--- composeApp/src/commonMain/kotlin/presentation/example/ExampleViewModel.kt
+++ composeApp/src/commonMain/kotlin/presentation/example/ExampleViewModel.kt
@@ -1,3 +1,29 @@
 package presentation.example
 
+import androidx.lifecycle.viewModelScope
+import data.example.ExampleRepository
+import toad.ToadViewModel
+
 //Relies on ExampleUiState, ExampleActionDependencies, ExampleAction
+
+class ExampleViewModel (
+    private val repository: ExampleRepository,
+    initialActions: List<ExampleAction> = emptyList()
+) : ToadViewModel<ExampleUiState, ExampleEvent>(
+    initialState = ExampleUiState(
+        id = "example",
+        toggle = false,
+        isLoading = false
+    )
+){
+    override val dependencies = ExampleActionDependencies(
+        exampleRepository = repository,
+        coroutineScope = viewModelScope,
+    )
+
+    init {
+        dispatchAll(initialActions)
+    }
+
+    fun runAction(action: ExampleAction) = dispatch(action)
+}
