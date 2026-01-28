b327f28b0eff6013edef30f074c112033e2ba7eb
Lindsay Martin <lindsayjamesmartin94@gmail.com>
2026-01-28 10:33:16 +0100

    Expand exampleDao interface to include all sorts of common CRUD operations

--- composeApp/src/commonMain/kotlin/data/example/source/local/ExampleDao.kt
+++ composeApp/src/commonMain/kotlin/data/example/source/local/ExampleDao.kt
@@ -1,10 +1,79 @@
 package data.example.source.local
 
+import kotlinx.coroutines.flow.Flow
+import org.mongodb.kbson.ObjectId
 
-sealed class ExampleDao {
-    data class Add(val example: ExampleEntityLocal) : ExampleDao()
+interface ExampleDao {
+    /**
+     * Observes list of example entities.
+     *
+     * @return all example entities.
+     */
+    fun observeAll(): Flow<List<ExampleEntityLocal>>
 
-    data class Update(val example: ExampleEntityLocal) : ExampleDao()
+    /**
+     * Observes a single entity.
+     *
+     * @param exampleId the id of the entity.
+     * @return the example entity with that exampleId.
+     */
+    fun observeById(exampleId: ObjectId): Flow<ExampleEntityLocal>
 
-    data class Delete(val example: ExampleEntityLocal) : ExampleDao()
+    /**
+     * Select all entities from the table.
+     *
+     * @return all entities.
+     */
+    suspend fun getAll(): List<ExampleEntityLocal>
+
+    /**
+     * Select an entity by id.
+     *
+     * @param exampleId the entity id.
+     * @return the entity with exampleId.
+     */
+    suspend fun getById(exampleId: ObjectId): ExampleEntityLocal?
+
+    /**
+     * Insert or update an example entity in the database. If an example entity already exists, replace it.
+     *
+     * @param example the example entity to be inserted or updated.
+     */
+    suspend fun upsert(example: List<ExampleEntityLocal>)
+
+    /**
+     * Insert or update example entities in the database. If an example entity already exists, replace it.
+     *
+     * @param examples the example entities to be inserted or updated.
+     */
+    suspend fun upsertAll(examples: List<ExampleEntityLocal>)
+
+    /**
+     * Update the value of a toggle in example
+     *
+     * @param exampleId id of the example entity
+     * @param toggleStatus value the toggle should have after updating
+     */
+    suspend fun updateToggle(exampleId: ObjectId, toggleStatus: Boolean)
+
+    /**
+     * Delete example entity by id.
+     *
+     * @return the number of entities deleted. This should always be 1.
+     */
+    suspend fun deleteById(exampleId: ObjectId): Int
+
+    /**
+     * Delete all example entities.
+     */
+    suspend fun deleteAll()
+
+
+    /**
+     * Delete all example entities with given toggle value.
+     *
+     * @param toggleStatus the value of the toggle to be removed
+     * @return the number of example entities deleted.
+     */
+    suspend fun deleteToggleTrue(toggleStatus: Boolean): Int
 }
