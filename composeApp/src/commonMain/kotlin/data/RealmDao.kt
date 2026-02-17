3cd3818a5ef6e02a53833fea26d6d931ead13e27
Lindsay Martin <lindsayjamesmartin94@gmail.com>
2026-02-17 15:58:11 +0100

    Clean up RealmDao more

--- composeApp/src/commonMain/kotlin/data/RealmDao.kt
+++ composeApp/src/commonMain/kotlin/data/RealmDao.kt
@@ -3,6 +3,7 @@ package data
 import data.tutorial.Database
 import io.realm.kotlin.Realm
 import io.realm.kotlin.notifications.ResultsChange
+import io.realm.kotlin.notifications.SingleQueryChange
 import io.realm.kotlin.query.RealmResults
 import io.realm.kotlin.types.RealmObject
 import kotlinx.coroutines.flow.Flow
@@ -13,11 +14,38 @@ import kotlin.reflect.KClass
 interface RealmDao<T : RealmObject> {
 
     // *C* reate ---------------------------------------------------------------------------
+
+    /**
+     * Insert or update an entity in the database. If an entity already exists, replace it.
+     *
+     * NOTE: Functionally the same as update
+     *
+     * @param entity the entity to be inserted or updated.
+     */
     suspend fun insert(entity: T)
+
+    /**
+     * Insert or update entities in the database. If an entity already exists, replace it.
+     *
+     * @param entities the entities that will be inserted or updated.
+     */
     suspend fun insertAll(entities: List<T>)
 
     // *R* ead -----------------------------------------------------------------------------
+
+    /**
+     * Select all entities from the table.
+     *
+     * @return all entities.
+     */
     suspend fun findAll(): RealmResults<T>
+
+    /**
+     * Select an entity by id.
+     *
+     * @param id the entity id.
+     * @return the entity with that id.
+     */
     suspend fun findById(id: ObjectId): T?
 
     /**
@@ -26,7 +54,7 @@ interface RealmDao<T : RealmObject> {
      * @param id the id of the entity.
      * @return flow of the T entity with that id.
      */
-    fun observeById(id: ObjectId): Flow<T>
+    fun observeById(id: ObjectId): Flow<SingleQueryChange<T>>
 
     /**
      * Observes change flow of T entities.
@@ -38,6 +66,14 @@ interface RealmDao<T : RealmObject> {
     fun observeAll(): Flow<ResultsChange<T>>
 
     // *U* pdate ---------------------------------------------------------------------------
+
+    /**
+     * Insert or update an entity in the database. If an entity already exists, replace it.
+     *
+     * NOTE: Functionally the same as insert
+     *
+     * @param entity the entity to be inserted or updated.
+     */
     suspend fun update(entity: T)
 
     // *D* elete ---------------------------------------------------------------------------
@@ -47,7 +83,7 @@ interface RealmDao<T : RealmObject> {
      *
      * @return the number of entities deleted. This should always be 1.
      */
-    suspend fun deleteById(exampleId: ObjectId): Int
+    suspend fun deleteById(id: ObjectId): Int
 
     /**
      * Delete T entity.
@@ -97,10 +133,11 @@ open class RealmDaoImpl<T : RealmObject>(
 //        return realm.query(clazz, "_id == $0", id).first().find()
         return realm.queryEqual(clazz, "_id", id).first().find()
         //Should field be _id or id? Is the example wrong or is this a quirk of ObjectId? Or PrimaryKey?
+        //Change method below if it does actually work.
     }
 
-    override fun observeById(id: ObjectId): Flow<T> {
-        TODO()
+    override fun observeById(id: ObjectId): Flow<SingleQueryChange<T>> {
+        return realm.queryEqual(clazz, "id", id).first().asFlow()
     }
 
     override fun observeAll(): Flow<ResultsChange<T>> {
@@ -109,7 +146,6 @@ open class RealmDaoImpl<T : RealmObject>(
 
     // *U* pdate ---------------------------------------------------------------------------
 
-    // Functionally the same as insert
     override suspend fun update(entity: T) {
         realm.write {
             copyToRealm(entity)
@@ -118,8 +154,14 @@ open class RealmDaoImpl<T : RealmObject>(
 
     // *D* elete ---------------------------------------------------------------------------
 
-    override suspend fun deleteById(exampleId: ObjectId): Int {
-        TODO()
+    override suspend fun deleteById(id: ObjectId): Int {
+        val toDelete = findById(id)
+        if (toDelete != null) {
+            delete(toDelete)
+            return 1
+        }
+        println("Couldn't find entity to delete with id: $id")
+        return 0
     }
 
     override suspend fun delete(entity: T) {
--- composeApp/src/commonMain/kotlin/data/example/source/local/ExampleDao.kt
+++ composeApp/src/commonMain/kotlin/data/example/source/local/ExampleDao.kt
@@ -9,35 +9,6 @@ import org.mongodb.kbson.ObjectId
 interface ExampleDao : RealmDao<ExampleEntityLocal> {
 
     /**
-     * Select all entities from the table.
-     *
-     * @return all entities.
-     */
-    suspend fun getAll(): List<ExampleEntityLocal>
-
-    /**
-     * Select an entity by id.
-     *
-     * @param exampleId the entity id.
-     * @return the entity with exampleId.
-     */
-    suspend fun getById(exampleId: ObjectId): ExampleEntityLocal?
-
-    /**
-     * Insert or update an example entity in the database. If an example entity already exists, replace it.
-     *
-     * @param example the example entity to be inserted or updated.
-     */
-    suspend fun upsert(example: List<ExampleEntityLocal>)
-
-    /**
-     * Insert or update example entities in the database. If an example entity already exists, replace it.
-     *
-     * @param examples the example entities to be inserted or updated.
-     */
-    suspend fun upsertAll(examples: List<ExampleEntityLocal>)
-
-    /**
      * Update the value of a toggle in example
      *
      * @param exampleId id of the example entity
@@ -57,22 +28,6 @@ interface ExampleDao : RealmDao<ExampleEntityLocal> {
 class ExampleDaoImpl(db: Database) :
     ExampleDao,
     RealmDaoImpl<ExampleEntityLocal> (db, ExampleEntityLocal::class) {
-
-    override suspend fun getAll(): List<ExampleEntityLocal> {
-        TODO()
-    }
-
-    override suspend fun getById(exampleId: ObjectId): ExampleEntityLocal? {
-        TODO()
-    }
-
-    override suspend fun upsert(example: List<ExampleEntityLocal>) {
-        TODO()
-    }
-
-    override suspend fun upsertAll(examples: List<ExampleEntityLocal>) {
-        TODO()
-    }
 
     override suspend fun updateToggle(exampleId: ObjectId, toggleStatus: Boolean) {
         TODO()
