a1851c016186896c4f761aed9f659454fd02a4bc
Lindsay Martin <lindsayjamesmartin94@gmail.com>
2026-06-16 00:19:11 +0200

    Remove SeedKey from external representation of Example

--- composeApp/src/commonMain/kotlin/data/example/Example.kt
+++ composeApp/src/commonMain/kotlin/data/example/Example.kt
@@ -1,15 +1,9 @@
 package data.example
 
-import data.SeedKey
-
 data class Example (
     val id: String,
     val toggle: Boolean,
 ){
     val isActive
         get() = toggle
-
-    companion object {
-        val SEED_EXAMPLE_ONE = SeedKey("example_one")
-    }
 }
