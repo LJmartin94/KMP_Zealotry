3fecaa5caf2e79e44c8f6e403943111fc75aae54
Lindsay Martin <lindsayjamesmartin94@gmail.com>
2026-07-03 02:01:23 +0200

    AI assisted - DayPartMenu restructure

--- composeApp/src/commonMain/kotlin/data/dayPartMenu/DayPartMenuRepository.kt
+++ composeApp/src/commonMain/kotlin/data/dayPartMenu/DayPartMenuRepository.kt
@@ -1,3 +1,3 @@
-package z.screens.dayPartMenu
+package data.dayPartMenu
 
-class DayPartMenuRepository
+interface DayPartMenuRepository
---
+++ composeApp/src/commonMain/kotlin/data/dayPartMenu/DayPartMenuRepositoryImpl.kt
@@ -0,0 +1,3 @@
+package data.dayPartMenu
+
+class DayPartMenuRepositoryImpl : DayPartMenuRepository
